334cd4baa5d06cd88440e5990d6bfed384780d49
xoten herself <ana.schotten@estudante.ifms.edu.br>
2024-09-30 18:34:11 -0300

    .

--- src/exemplo2/index.jsx
+++ src/exemplo2/index.jsx
@@ -5,22 +5,22 @@ export default function App() {
   const [fotos, setFotos] = useState([]);
 
   useEffect(() => { 
-    const buscarUsuario  = async () => {
+    const buscarFotos  = async () => {
         const resposta = await fetch('https://jsonplaceholder.typicode.com/photos');
         const dados = await resposta.json();
         setFotos(dados);
     }
-    buscarUsuario();
+    buscarFotos();
   }, []);
 
   return (
     <>
       <h1>Galeria de Fotos</h1>
       <ul>
-        {fotos.map(photo => (
-          <li key={photo.id}>
-            <h2>{photo.title}</h2>
-            <img src={photo.image} alt={photo.title} width={100} />
+        {fotos.map(foto => (
+          <li key={foto.id}>
+            <h2>{foto.title}</h2>
+            <img src={foto.url} alt={foto.title} width={100} />
           </li>
         ))}
       </ul>
--- src/exemplo3/index.jsx
+++ src/exemplo3/index.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 export default function App() {
   
-  const [user, setUser] = useState([]);
+  const [user, setUser] = useState({});
 
   useEffect(() => { 
     const receberDadosUser = async () => {
@@ -17,13 +17,12 @@ export default function App() {
     <>
       <h1>Usuário</h1>
       <ul>
-        {user.map(user => (
-          <li key={user.id}>
-            <img src={user.image} alt={user.name} width={100} />
-            <h2>{user.name}</h2>
-            <p>{user.description}</p>
-          </li>
-        ))}
+        <li>Gênero: {user?.resposta[0].gender}</li>
+        <li>Nome: {user?.resposta[0].name}</li>
+        <li>Endereço: {user?.resposta[0].location}</li>
+        <li>E-mail: {user?.resposta[0].email}</li>
+        <li>Login: {user?.resposta[0].login}</li>
+        <li>Info: {user?.resposta[0].info}</li>
       </ul>
     </>
   );
